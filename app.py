02d4da33a95ec791b49be8f3ddafe4944b22df6d
patellikitha <patellikitha67@gmail.com>
2026-06-10 17:13:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ def home():
     )
 
 
-# Enqueue customer
+# ---------------- QUEUE ----------------
+
 @app.route("/enqueue", methods=["POST"])
 def enqueue():
     global last_operation
@@ -38,7 +39,6 @@ def enqueue():
     return redirect(url_for("home"))
 
 
-# Dequeue customer
 @app.route("/dequeue")
 def dequeue():
     global last_operation
@@ -48,6 +48,36 @@ def dequeue():
         last_operation = f"Ticket booked for {customer}."
     else:
         last_operation = "Booking queue is empty."
+
+    return redirect(url_for("home"))
+
+
+# ---------------- STACK ----------------
+
+@app.route("/push", methods=["POST"])
+def push():
+    global last_operation
+
+    ticket = request.form.get("ticket")
+
+    if ticket:
+        ticket_stack.append(ticket)
+        last_operation = f"Ticket {ticket} added to the stack."
+    else:
+        last_operation = "Please enter a ticket ID."
+
+    return redirect(url_for("home"))
+
+
+@app.route("/pop")
+def pop():
+    global last_operation
+
+    if ticket_stack:
+        ticket = ticket_stack.pop()
+        last_operation = f"Ticket {ticket} removed from the stack."
+    else:
+        last_operation = "Ticket stack is empty."
 
     return redirect(url_for("home"))
 
