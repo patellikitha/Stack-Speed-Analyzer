58cd53d84e31ce9da83b794a91dba09f2ae18726
patellikitha <patellikitha67@gmail.com>
2026-06-10 15:07:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 
-# app.py
-
 from flask import Flask, render_template
 from collections import deque
 
 app = Flask(__name__)
 
-# Data structures
+# Queue and Stack data structures
 booking_queue = deque()
 ticket_stack = []
 
-# Message to display on the home page
-last_operation = "🎬 Welcome to Movie Ticket Booking System!"
+# Default message
+last_operation = "Welcome to the Movie Ticket Booking System!"
 
 
 @app.route("/")
